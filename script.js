423a15f695c77c3601eb2a486540cf7d3a0e5ca0
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-08-14 08:50:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+ function submitForm() {
+            const firstName = document.getElementById("first-name").value;
+            const lastName = document.getElementById("last-name").value;
+            const phoneNumber = document.getElementById("phone-number").value;
+            const email = document.getElementById("email").value;
+
+            const formInfo = `
+            First Name: ${firstName}
+            Last Name: ${lastName}
+            Phone Number: ${phoneNumber}
+            Email ID: ${email}
+            `;
+
+            alert(formInfo);
+        }
